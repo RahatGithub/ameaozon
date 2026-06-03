dfe7bd9d09b4dc0a3d693e04a9f6042ec2aaa8e1
Rahat Ahmed Chowdhury <61385277+RahatGithub@users.noreply.github.com>
2026-06-03 13:37:51 +0600

    fix(auth): add autocomplete="off" to password fields in login and register forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,10 +4,15 @@ from .models import User
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['autocomplete'] = 'off'
+        self.fields['password2'].widget.attrs['autocomplete'] = 'off'
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
